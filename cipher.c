43a13eccecf6a6c78d1b546788edbe9f4801d09b
Gene Schroer <greatgadolinium@gmail.com>
2016-02-14 16:13:07 -0500

    Hardcode a simpleread/write interface

--- cipher.c
+++ cipher.c
@@ -8,6 +8,8 @@
 			//access, getpagesize
 
 
+#define BUFSIZE 512 // Size of the buffer to read data
+
 /* Uses the example provided in the gnu library to implement the getopt
  *
  *
@@ -19,7 +21,7 @@
  */
 int main(int argc, char **argv){
 
-
+char buf[BUFSIZE];
 
 //char *infile = argv[1];
 struct stat *instat = malloc(sizeof(stat));
@@ -31,19 +33,62 @@ if(argc < 3){
 else{
 
 
-const char *inpath = "/home/work/CSE_376/HW1/input.txt";
-int fd;
+
+
+
+/* use stat later for error checking*/
 // if(stat(inpath, instat) == -1){
 // 	printf("Not a valid file\n");
 // 	exit(2);
 // }
 
-if((fd = open(inpath, O_RDONLY))== -1){
+
+/* Check to see if both the input and output files exist/can be opened*/
+const char *inpath = "/home/work/CSE376/HW1/input.txt";
+int fd_in;
+if((fd_in = open(inpath, O_RDONLY))== -1){
 	printf("Not a valid file\n");
 	exit(2);
 }
+printf("Success! Input file has been opened");
 
-printf("Success! Your file descriptor number is: %d\n", fd);
+/*Check if output file exists
+ * If no, create it
+ *
+ * If yes, is it writeable?
+ * If not, return error
+ */
+
+const char *outpath = "/home/work/CSE376/HW1/output.txt";
+int fd_out;
+if((fd_out = open(outpath, O_WRONLY | O_CREAT, S_IRWXU))== -1){
+	printf("Error! Output file cannot be opened\n");
+	exit(2);
+}
+
+
+
+
+int readsize = BUFSIZE;
+
+int bytes_cpyd;
+
+while((bytes_cpyd = read(fd_in, buf, readsize)) > 0){
+	if(write(fd_out, buf, bytes_cpyd) < 0)
+		exit(EXIT_FAILURE);	
+
+}
+
+/* Close the input/output files*/
+
+if(close(fd_in) < 0){
+	exit(EXIT_FAILURE);
+}
+
+if(close(fd_out) < 0){
+	exit(EXIT_FAILURE);
+}
+
 
 int args;
 
